44873e2e4c1b5d2daf3f3a2b993831799029c50b
itsmetohirr <itsmetohir@gmail.com>
2025-03-09 09:03:13 +0500

    room-form done

--- studycom/views.py
+++ studycom/views.py
@@ -108,14 +108,20 @@ def create_room(request):
     form = RoomForm()
     if request.method == "POST":
         form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
             
-            return redirect('home')
+        return redirect('home')
 
-    context = {'form': form}
+    topics = Topic.objects.all()
+    context = {'form': form, 'topics': topics}
     return render(request, 'studycom/room_form.html', context)
 
 
@@ -128,12 +134,17 @@ def update_room(request, pk):
         return HttpResponse('You are not allowed to edit')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        
+        return redirect('home')
 
-    context = {'form': form}
+    topics = Topic.objects.all()
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request, 'studycom/room_form.html', context)
 
 
