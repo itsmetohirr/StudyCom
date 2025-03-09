fa3a64ebf6bcbafc0888e903681b30a2c91d4d9f
itsmetohirr <itsmetohir@gmail.com>
2025-03-09 12:10:28 +0500

    avatar set

--- studycom/models.py
+++ studycom/models.py
@@ -6,9 +6,11 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True)
     bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default='avatar.svg')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
 
 
 class Topic(models.Model):
