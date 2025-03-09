2bf03749458409224ad3f28ed8b2ba7d57ae5f36
itsmetohirr <itsmetohir@gmail.com>
2025-03-09 11:24:02 +0500

    api-done

--- root/settings.py
+++ root/settings.py
@@ -41,8 +41,12 @@ INSTALLED_APPS = [
     'studycom.apps.StudycomConfig',
 
     'rest_framework',
+
+    'corsheaders',
     
 ]
+
+AUTH_USER_MODEL = 'studycom.'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
@@ -133,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
