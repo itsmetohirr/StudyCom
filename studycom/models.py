4f85e8006d8c6d66b6e69cff5ccc7139b9128dce
itsmetohirr <itsmetohir@gmail.com>
2025-03-09 11:38:35 +0500

    custom user fixed

--- studycom/models.py
+++ studycom/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Topic(models.Model):
